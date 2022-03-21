54386320af0044bd7488935e4eaf6f74512e286e
Sane21 <readbook2002h14@gmail.com>
2022-03-22 03:45:17 +0900

    弾数と方向
    6方向

--- Survivor/Assets/Scripts/Strategy/WaterStrategy.cs
+++ Survivor/Assets/Scripts/Strategy/WaterStrategy.cs
@@ -19,6 +19,11 @@ public class WaterStrategy : GeneratorStrategy
     public void Attack()
     {
         this.generator.Shot(); // 上向きに射撃
+        this.generator.Shot(3, 1, 33.33f, 3);
+        this.generator.Shot(-3, 1, 153.33f, 3);
+        this.generator.Shot(3, -1, -33.33f, 3);
+        this.generator.Shot(-3, -1, -153.33f, 3);
+        this.generator.Shot(0, -1, -90, 10);
     }
     /// <summary>
     /// 発電所の射撃間隔の確定
