702ecb2fea5f6eb48337e38a2e4bfd253c055720
husky <68993961+husky21st@users.noreply.github.com>
2022-03-24 16:19:53 +0900

    Revert "風力発電作成"

--- Survivor/Assets/Scripts/Strategy/WindStrategy.cs
+++ Survivor/Assets/Scripts/Strategy/WindStrategy.cs
@@ -1,6 +1,3 @@
- using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
 /// <summary>
 /// 風力
 /// </summary>
@@ -8,14 +5,12 @@ public class WindStrategy : GeneratorStrategy
 {
     private Generator generator;
     private string name = "風力発電所";
-    private int interval;
+    private int interval = 5;
     private int hp = 3;
-    private float rad;
 
     public WindStrategy()
     {
-        this.interval = 3;
-        this.rad = -Mathf.PI/4;
+
     }
 
     /// <summary>
@@ -23,15 +18,7 @@ public class WindStrategy : GeneratorStrategy
     /// </summary>
     public void Attack()
     {
-        if (Generator.GetPhase() == 0)
-        {
-            this.rad += Mathf.PI/4;
-        }
-        else
-        {
-            this.rad += Mathf.PI/8;
-        }
-        this.generator.Shot(Mathf.Cos(this.rad), Mathf.Sin(this.rad), 1, 10);
+        this.generator.Shot(); // 上向きに射撃
     }
     /// <summary>
     /// 発電所の射撃間隔の確定
@@ -43,18 +30,6 @@ public class WindStrategy : GeneratorStrategy
             return;
         }
 
-        if (Generator.GetPhase() == 0)
-        {
-            this.interval = 3;
-        }
-        else if (Generator.GetPhase() == 1)
-        {
-            this.interval = 2;
-        }
-        else if (Generator.GetPhase() == 2)
-        {
-            this.interval = 1;
-        }
         this.generator.SetInterval(this.interval);
     }
     /// <summary>
