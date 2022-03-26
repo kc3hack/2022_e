3aefde187446d060b559195cdccc00c827ceb30f
Ikegami-Takuto <uyc7vh28@outlook.jp>
2022-03-26 17:45:21 +0900

    難易度の調整

--- Survivor/Assets/Scripts/Planter/ThirdPlantStrategy.cs
+++ Survivor/Assets/Scripts/Planter/ThirdPlantStrategy.cs
@@ -13,7 +13,7 @@ public class ThirdStratgey : PlanterStrategy
     private List<string> target = new List<string>();
     private int length = 3;
     private System.Random r = new System.Random();
-    private float[] generateSpeed = {6.0f, 6.0f, 6.0f};//SetSecondの引数
+    private float[] generateSpeed = {6.5f, 6.5f, 6.5f};//SetSecondの引数
     public void Initialize()
     {
         //家電を配列に入れる
@@ -51,10 +51,10 @@ public class ThirdStratgey : PlanterStrategy
             x = plantPositionX + (r.Next(-500, 500) + r.Next(-90, 90) + r.Next(-10, 10));
             y = plantPositionY + (r.Next(-500, 500) + r.Next(-90, 90) + r.Next(-10, 10));
 
-            if(Math.Pow(x - plantPositionX, 2) + Math.Pow(y - plantPositionY, 2) > 490000){
+            if(Math.Pow(x - plantPositionX, 2) + Math.Pow(y - plantPositionY, 2) > 640000){
                 continue;
             }
-            if(Math.Pow(x - plantPositionX, 2) + Math.Pow(y - plantPositionY, 2) > 160000){
+            if(Math.Pow(x - plantPositionX, 2) + Math.Pow(y - plantPositionY, 2) > 360000){
                 Debug.Log("(" + x.ToString() + "," + y.ToString() + ")につくる");
                 break;
             }
